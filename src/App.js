569aa258bea73faf6466813ec000c13e078d809e
anamika <anamika.bhardwaj@thoughtworks.com>
2024-01-02 00:22:54 +0530

    AB: Setup react route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
-
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+//should only contain  very hgh level stuff in this file
 function App() {
   return (
-    <div className="App">
-      <p>Amazon clone</p>
-    </div>
+    <Router>
+        <div className="app">
+            <h1>Build amazon clone</h1>
+            <Routes>
+                <Route path="/checkout" element={<h1>checkout</h1>}>
+
+                </Route>
+                <Route path="/login" element={<h1>Login</h1>}>
+
+                </Route>
+                <Route path="/" element={<h1>Default page</h1>} >
+
+                </Route>
+            </Routes>
+        </div>
+    </Router>
   );
 }
 
