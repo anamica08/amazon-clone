b4e96c39b958cb488892d2949066323d821bd119
anamika <anamika.bhardwaj@thoughtworks.com>
2024-01-02 17:35:21 +0530

    AB: Created Banner

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import Header from './components/Header';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
 //should only contain  very hgh level stuff in this file
 function App() {
   return (
     <Router>
-        <div className="app">
-            <Routes>
-                <Route path="/checkout" element={<h1>checkout</h1>}>
-
-                </Route>
-                <Route path="/login" element={<h1>Login</h1>}>
-
-                </Route>
-                <Route path="/" element={
-                  <Header/>
-                } >
-
-                </Route>
-            </Routes>
-        </div>
+      <div className="app">
+        <Routes>
+          <Route
+            path="/checkout"
+            element={
+              <>
+                <Header />
+                <h1>checkout</h1>
+              </>
+            }
+          ></Route>
+          <Route path="/login" element={<h1>Login</h1>}></Route>
+          <Route path="/" element={
+          <>
+             <Header />
+             <Home />
+          </>
+       }></Route>
+        </Routes>
+      </div>
     </Router>
   );
 }
